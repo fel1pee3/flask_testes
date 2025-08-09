279db04c599ef2cbb40501459a44927ca98d231a
Felipe Maia <132314205+fel1pee3@users.noreply.github.com>
2025-08-09 03:28:02 -0300

    atz: adição de integração com um bd(mysqlLite)

--- estudo/__init__.py
+++ estudo/__init__.py
@@ -1,5 +1,13 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate, migrate
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 from estudo.views import home
+from estudo.models import Contato
