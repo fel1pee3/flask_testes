67f39296ddafa9a93fd26ae828a038ecc7f5b423
Felipe Maia <jofelipefm@gmail.com>
2025-08-15 13:42:27 -0300

    feat: form get e post simples adicionadados

--- estudo/views.py
+++ estudo/views.py
@@ -1,6 +1,6 @@
-from estudo import app
-from flask import render_template, url_for
-
+from estudo import app, db
+from flask import render_template, url_for, request
+from estudo.models import Contato
 @app.route('/')
 def home():
     usuario = 'José Felipe Fernandes Maia'
@@ -11,6 +11,28 @@ def home():
     }
     return render_template('index.html', context=context)
 
-@app.route('/outrarota/')
+@app.route('/contato/', methods=['GET', 'POST'])
 def rotadois():
-    return 'Outra Rota'
+    context = {}
+    if request.method == 'GET':
+        pesquisa = request.args.get('pesquisa')
+        context.update({'pesquisa': pesquisa})
+        print('GET', pesquisa)
+
+    if request.method == 'POST':
+        nome = request.form['nome']
+        email = request.form['email']
+        assunto = request.form['assunto']
+        mensagem = request.form['mensagem']
+
+        contato = Contato(
+            nome=nome,
+            email=email,
+            assunto=assunto,
+            mensagem=mensagem
+        )
+
+        db.session.add(contato)
+        db.session.commit()
+
+    return render_template('contato.html', context=context)
